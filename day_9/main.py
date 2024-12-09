70800a600135335206e9c43ff7119ada51bacb04
dmitriidrozdov <mitya.drozdov@gmail.com>
2024-12-09 22:56:58 +0400

    day 9 p2 completed. Fixed missing constrain for window=0

--- day_9/main.py
+++ day_9/main.py
@@ -54,11 +54,11 @@ def main_2(path=_path + "/input.txt", print_value=True):
             ix = i//2
             decripted.extend([ix for _ in range(int(char))])
         else:
-            decripted.extend(['' for _ in range(int(char))])
+            decripted.extend(['.' for _ in range(int(char))])
 
     reversed_decripted = decripted[::-1]
     unique_values = list(set(reversed_decripted))
-    unique_values.remove('')
+    unique_values.remove('.')
     vpos = {}
     for v in unique_values:
         start = decripted.index(v)
@@ -68,26 +68,26 @@ def main_2(path=_path + "/input.txt", print_value=True):
     for v in tqdm(sorted(unique_values, reverse=True)):
         size = vpos[v][1] - vpos[v][0] + 1
         window = 0
-        first_empty_ind = decripted.index('')
+        first_empty_ind = decripted.index('.')
         if size+first_empty_ind <= len(decripted):
             for i in range(first_empty_ind, size+first_empty_ind):
-                if decripted[i] != '':
+                if decripted[i] != '.':
                     window += 1
-            if window == 0:
+            if (window == 0) and (vpos[v][0] > first_empty_ind + size - 1):
                 for i in range(first_empty_ind, size+first_empty_ind):
                     j = i - first_empty_ind
                     decripted[i], decripted[vpos[v][0] + j] = decripted[vpos[v][0] + j], decripted[i]
             else:
                 for i in range(1+first_empty_ind, vpos[v][0]-size+1):
-                    next_window_change = 0 if decripted[i + size - 1] == '' else 1
-                    prev_window_change = 0 if decripted[i-1] == '' else 1
+                    next_window_change = 0 if decripted[i + size - 1] == '.' else 1
+                    prev_window_change = 0 if decripted[i-1] == '.' else 1
                     window += next_window_change - prev_window_change
                     if window == 0:
                         for j in range(size):
                             decripted[i+j], decripted[vpos[v][0] + j] = decripted[vpos[v][0] + j], decripted[i+j]
                         break
                 
-    res = sum([i*v for i, v in enumerate(decripted) if v != ''])
+    res = sum([i*v for i, v in enumerate(decripted) if v != '.'])
 
     if print_value:
         print(res)
@@ -103,12 +103,17 @@ def check_example(func, input_filename = "example_input.txt", answer_filename = 
     ), f"Calculation {example_calculation} is different from example answer {example_answer}"
 
 if __name__ == "__main__":
-    # check_example(main, input_filename = "example_input.txt", answer_filename = "example_answer.txt")
+    check_example(main, input_filename = "example_input.txt", answer_filename = "example_answer.txt")
 
-    # #run on the input
-    # main()
+    #run on the input
+    main()
 
     check_example(main_2, input_filename = "example_input.txt", answer_filename = "example_answer_2.txt")
+
+    #run on the input
+    main_2()
+
+    check_example(main_2, input_filename = "example_input_3.txt", answer_filename = "example_answer_3.txt")
 
     #run on the input
     main_2()
